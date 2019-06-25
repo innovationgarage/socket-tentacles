fee1db2080cf1e2820d85114377507b2b2c2a129
Egil <egil.moller@freecode.no>
2019-06-25 18:35:37 +0200

    Bugfix for homepage url

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 import setuptools
 
 setuptools.setup(name='socket-tentacles',
-      version='0.0.2',
+      version='0.0.3',
       description="""Simple tcp client/server that divorces the concept of server/client from which side opens the tcp connection.""",
       long_description="""Simple thread based tcp client/server that divorces the concept of server/client from which side opens the tcp connection. Configured using a simple dictionary structure that can be easily read from json.""",
       author='Egil Moeller',
       author_email='egil@innovationgarage.no',
-      url='https://github.com/innovationgarage/tentacles',
+      url='https://github.com/innovationgarage/socket-tentacles',
       packages=setuptools.find_packages()
   )
