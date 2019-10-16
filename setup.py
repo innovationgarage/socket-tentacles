87e35ef34584d458f17fc127e7360e7a194de0f2
Egil <egil.moller@freecode.no>
2019-10-16 10:49:36 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 setuptools.setup(name='socket-tentacles',
-      version='0.0.7',
+      version='0.0.8',
       description="""Simple tcp client/server that divorces the concept of server/client from which side opens the tcp connection.""",
       long_description="""Simple thread based tcp client/server that divorces the concept of server/client from which side opens the tcp connection. Configured using a simple dictionary structure that can be easily read from json.""",
       author='Egil Moeller',
